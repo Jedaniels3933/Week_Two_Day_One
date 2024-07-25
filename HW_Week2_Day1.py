8706bada91f50d15e380be24ed91be2bff250604
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-07-24 23:39:14 -0400

    2

--- HW_Week2_Day1.py
+++ HW_Week2_Day1.py
@@ -1,28 +1,10 @@
-library = []
+shopping_list = []
+user_input = input("Enter an item here")
+print(user_input)
+shopping_list.append(user_input)
+print(shopping_list)
 
-def add_to_library(grocery):  
-    grocery = input("What is the name of item?")
-    library.append(grocery)
-    return library
 
-def main():
-    grocery_list = []
-    flag = True
-    while flag:
-        ans = input('''
-What would you like to do?)
-1. Add a new item to list
-2. View list   
-3. Quit
-''')
-        if ans == '1':
-            updated_library = add_to_library(library)
-        elif ans == '2':
-            library()
-        elif ans == '3':
-            flag = False
-            print("Thank you for using this app!")  
-main()
- 
 
  
+
