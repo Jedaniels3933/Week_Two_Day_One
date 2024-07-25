54858f8d3c6ddfc652c7de307d80b1f810b1f262
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-07-25 01:40:09 -0400

    Create After_Practice.py

---
+++ After_Practice.py
@@ -0,0 +1,7 @@
+
+books = "THe Green Mile", "American Gods",
+def add_to_library(author, title):
+    book = [author, title]
+    books.append(book)
+    print(f"Book '{title}' by {author} added to the library.")
+    
